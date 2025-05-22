c59b7fa9988b9b9bdc0d4f548283f2acee58ccd0
YARED KEBEDE <21bcs10676@cuchd.in>
2025-05-22 10:56:43 +0000

    templates

--- blango/settings.py
+++ blango/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
